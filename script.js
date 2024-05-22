d8fc640b75c2960b50ebb6cbe063a21779df4178
null65 <jackbrown20111@gmail.com>
2024-05-22 21:57:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function navigateToHomePage() {
+  window.location.href = "index.html";
+}
+
+function navigateToExtraPage() {
+  window.location.href = "Projects.html";
+}
+
+function notcool() {
+  window.location.href = "setup.html";
+}
+
+function navigateToHomeprepage() {
+  window.location.href = "setup.html";
+}
+
+function navigateToAnotherPage() {
+  window.location.href = "<-- Back to Home Page";";
+}
+
+
